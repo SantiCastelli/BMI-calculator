3e99137ac7f01f68611e7df818e12b52503557ca
SantiCastelli <castellist@gmail.com>
2022-03-16 13:38:00 +0100

    terminado

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 // Implementar el código aqui
 
 // Escuchar el evento 'submit'
+let calcular = document.forms["calculator"];
+console.log(calcular);
+
+calcular.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("hola");
+
+    let height = document.querySelector("#height").value;
+    console.log(height);
+
+    let weight = document.querySelector("#weight").value;
+    console.log(weight);
+
+    let imc = (weight / ((height*height)/10000)).toFixed(2);
+    console.log(imc);
+
+    document.querySelector("#results").textContent = imc
+})
